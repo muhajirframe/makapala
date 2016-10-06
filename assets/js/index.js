8cbf6e947ab95dbd43244bf785de8a14761441a1
Muhammad Muhajir <muhajirframe@gmail.com>
2016-10-06 22:02:37 +0700

    add scrollReveal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,9 @@
-console.log('you can use ES6 here : )')
+import ScrollReveal from 'scrollreveal'
+
+window.sr = ScrollReveal()
+sr.reveal('p')
+sr.reveal('#header-container > *', 500)
+sr.reveal('#about img', 500)
+sr.reveal('.event-box', 200)
+sr.reveal('fieldset > *', 150)
+sr.reveal('#contacts-list div', 200)
