f6eb87aeac8ba619d026ec6cf2f4128df091cb88
Muhammad Muhajir <muhajirframe@gmail.com>
2016-10-15 21:13:07 +0700

    add page visi-misi and prestasi, add _footer.sgr

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@ import ScrollReveal from 'scrollreveal'
 
 window.sr = ScrollReveal()
 sr.reveal('p')
+sr.reveal('h1')
+sr.reveal('h3')
 sr.reveal('#header-container > *', 500)
 sr.reveal('#about img', 500)
 sr.reveal('.event-box', 200)
